312978c382ce93823bb1cf8af3388da782a85dd8
Glenn Robinson <Glenn.Robinson@rsipvm.com>
2026-05-06 21:33:13 +0100

    Jest for testing

---
+++ jest.config.cjs
@@ -0,0 +1,7 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/src/robot/__tests__/**/*.test.ts'],
+  verbose: true,
+};
