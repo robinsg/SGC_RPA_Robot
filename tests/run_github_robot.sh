da616b6c86e9d61d227cfe79739d72e3f42b2497
Glenn Robinson <Glenn.Robinson@rsipvm.com>
2026-06-12 11:30:07 +0100

    Log levfel test

--- tests/run_github_robot.sh
+++ tests/run_github_robot.sh
@@ -59,6 +59,7 @@ LOG_LEVEL="${PARAMS[3]}"
 echo "=========================================================="
 echo "Executing robotic tests for: $VARIANT_KEY"
 echo "Using parameters: --yaml-file $YAML_FILE --host [HIDDEN] --env [HIDDEN]"
+echo "Log level: $LOG_LEVEL"
 echo "=========================================================="
 
 # Ensure execution permissions on the original script
